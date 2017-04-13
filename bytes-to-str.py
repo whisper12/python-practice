afb39af5b11656de283f48a3b35636f22847d015
zhouqiaobo <zhouqiaobo@pipi.cn>
2017-04-13 15:32:38 +0800

    note

---
+++ bytes-to-str.py
@@ -0,0 +1,18 @@
+# bytes object  
+ b = b"example"  
+  
+ # str object  
+ s = "example"  
+  
+ # str to bytes  
+ bytes(s, encoding = "utf8")  
+  
+ # bytes to str  
+ str(b, encoding = "utf-8")  
+  
+ # an alternative method  
+ # str to bytes  
+ str.encode(s)  
+  
+ # bytes to str  
+ bytes.decode(b)  
